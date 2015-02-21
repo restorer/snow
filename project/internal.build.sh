08749518eb8975087ce25019a7176065cb500d96
underscorediscovery <sven@underscorediscovery.com>
2015-02-21 02:22:03 -0330

    build; add internal file for CI testing

---
+++ project/internal.build.sh
@@ -0,0 +1,7 @@
+
+#This file is used for continuous integration only and shouldn't be called directly.
+#To build manually read the snow docs and run `flow build --options` as usual.
+# https://underscorediscovery.github.io/snow/guide/native-layer.html
+
+flow build
+flow build --d snow_dynamic_link
