aa5c80a014873d04ea7f7ca91b52297ee32af0a9
underscorediscovery <sven@underscorediscovery.com>
2015-02-21 02:37:42 -0330

    build; ci; clean up script more

--- project/internal.build.sh
+++ project/internal.build.sh
@@ -3,7 +3,15 @@
 #To build manually read the snow docs and run `flow build --options` as usual.
 # https://underscorediscovery.github.io/snow/guide/native-layer.html
 
+#ensure dependencies are down
+git submodule update --init
+
+#jump to correct folder for builds
 cd project
-flow build
+#build host arch, static
+flow build --d static_link
+#build dynamic
 flow build --d snow_dynamic_link
+
+#revert to play nice
 cd ..
