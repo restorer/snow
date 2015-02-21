2c8ac66027e82b2c2bbdbba5b70b9e131517f8e2
underscorediscovery <sven@underscorediscovery.com>
2015-02-21 03:40:21 -0330

    build; ci; env based control of builds for more targets

--- project/internal.build.sh
+++ project/internal.build.sh
@@ -11,10 +11,27 @@ git submodule update --init
 
 #jump to correct folder for builds
 cd project
-#build host arch, static
-haxelib run flow build --d static_link --log $SNOW_BUILD_LOG_LEVEL
-#build dynamic
-haxelib run flow build --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+
+#64 bit if specified
+if [ -n "$SNOW_BUILD_ARCH_64" ] then
+    haxelib run flow build --arch 64 --d static_link --log $SNOW_BUILD_LOG_LEVEL
+    haxelib run flow build --arch 64 --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+fi
+
+if [ -n "$SNOW_BUILD_ARCH_32" ] then
+    haxelib run flow build --arch 32 --d static_link --log $SNOW_BUILD_LOG_LEVEL
+    haxelib run flow build --arch 32 --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+fi
+
+if [ -n "$SNOW_BUILD_ANDROID" ] then
+    haxelib run flow build android --arch armv6 --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+    haxelib run flow build android --arch armv7 --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+    haxelib run flow build android --arch x86 --d snow_dynamic_link --log $SNOW_BUILD_LOG_LEVEL
+fi
+
+if [ -n "$SNOW_BUILD_IOS" ] then
+    haxelib run flow build ios --archs armv6,armv7,armv7s,arm64,sim,sim64 --d static_link --log $SNOW_BUILD_LOG_LEVEL
+fi
 
 #revert to play nice
 cd ..
