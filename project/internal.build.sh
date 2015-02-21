69c60dd07803198612aa9430acc30670ce2797f6
underscorediscovery <sven@underscorediscovery.com>
2015-02-21 02:32:37 -0330

    build; ci; fix paths

--- project/internal.build.sh
+++ project/internal.build.sh
@@ -3,5 +3,7 @@
 #To build manually read the snow docs and run `flow build --options` as usual.
 # https://underscorediscovery.github.io/snow/guide/native-layer.html
 
+cd project
 flow build
 flow build --d snow_dynamic_link
+cd ..
