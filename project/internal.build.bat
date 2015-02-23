2d4590fae5a654038902e0e29fd8c9696ec37549
underscorediscovery <sven@underscorediscovery.com>
2015-02-22 20:46:18 -0330

    build; ci; initial test flags

--- project/internal.build.bat
+++ project/internal.build.bat
@@ -4,3 +4,12 @@ echo "snow; Don't run this script directly, read inside the file for why."
 REM To build manually read the snow docs and run `flow build --options` as usual.
 REM https://underscorediscovery.github.io/snow/guide/native-layer.html
 
+if defined %SNOW_BUILD_ARCH_32% (
+
+    echo "snow; build; running arch 32..."
+    haxelib run flow build --project project/snow.flow --arch 32 --d static_link --log "%SNOW_BUILD_LOG_LEVEL%"
+    haxelib run flow build --project project/snow.flow --arch 32 --d snow_dynamic_link --log "%SNOW_BUILD_LOG_LEVEL%"
+
+)
+
+
