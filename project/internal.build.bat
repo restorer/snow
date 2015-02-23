2a7ecc793430453c369db739707abb35f3b37a6f
underscorediscovery <sven@underscorediscovery.com>
2015-02-22 20:42:00 -0330

    build; ci; add windows script

---
+++ project/internal.build.bat
@@ -0,0 +1,6 @@
+
+REM This file is used for continuous integration only and shouldn't be called directly.
+echo "snow; Don't run this script directly, read inside the file for why."
+REM To build manually read the snow docs and run `flow build --options` as usual.
+REM https://underscorediscovery.github.io/snow/guide/native-layer.html
+
